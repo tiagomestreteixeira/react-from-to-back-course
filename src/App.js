592080fb49d39e8346298ea8d1f6aa2467f557cc
Tiago Mestre Teixeira <tam.teixeira@gmail.com>
2018-08-12 22:00:03 +0100

    Context Api

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contacts from './components/contacts';
 
+import { Provider } from './context';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <Header />
-        <div className="container">
-          <Contacts />
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <Header />
+          <div className="container">
+            <Contacts />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 import './contact.css';
 
 class Contact extends Component {
@@ -11,44 +12,51 @@ class Contact extends Component {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   };
 
-  onDeleteClick = () => {
+  onDeleteClick = (id, dispatch) => {
     console.log('clicked');
-    this.props.deleteClickHandler();
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
-    return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{' '}
-          <i
-            onClick={this.onShowClick}
-            className="fas fa-sort-down"
-            style={{ cursor: 'pointer' }}
-          />
-          <i
-            className="fas fa-times"
-            style={{ cursor: 'pointer', float: 'right', color: 'red' }}
-            onClick={this.onDeleteClick}
-          />
-        </h4>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
 
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired,
-  deleteClickHandler: PropTypes.func.isRequired
+  contact: PropTypes.object.isRequired
 };
 
 export default Contact;
