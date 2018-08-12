2046323b161f61a6b748d779a0e7f28d2f2f7b1a
Tiago Mestre Teixeira <tam.teixeira@gmail.com>
2018-08-12 17:46:34 +0100

    set state

---
+++ src/components/contacts.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Contact from './contact';
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'Karen williams',
+        email: 'karen@gmail.com',
+        phone: '222-222-2222'
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'henry@gmail.com',
+        phone: '111-111-1111'
+      }
+    ]
+  };
+
+  deleteContact = id => {
+    console.log(`deleting id: ${id}`);
+    const { contacts } = this.state;
+    const newContacts = contacts.filter(contact => contact.id !== id);
+    this.setState({ contacts: newContacts });
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <React.Fragment>
+        {contacts.map(contact => (
+          <Contact
+            key={contact.id}
+            contact={contact}
+            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+          />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contacts;
---
+++ src/context.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+export class Provider extends Component {}
